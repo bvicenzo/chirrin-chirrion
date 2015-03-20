9043b5f73620ef34dd1337f323d4389b8be6b2e1
Bruno Vicenzo <greenmetal@gmail.com>
2015-03-20 15:12:22 -0300

    built skeleton

---
+++ lib/chirrin/chirrion.rb
@@ -0,0 +1,7 @@
+require "chirrin/chirrion/version"
+
+module Chirrin
+  module Chirrion
+    # Your code goes here...
+  end
+end
---
+++ lib/chirrin/chirrion/version.rb
@@ -0,0 +1,5 @@
+module Chirrin
+  module Chirrion
+    VERSION = "0.1.0"
+  end
+end
