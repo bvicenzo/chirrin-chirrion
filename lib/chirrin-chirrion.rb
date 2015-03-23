8a6a8f74f67534f2867ed02a29adb76001262305
Bruno Vicenzo <greenmetal@gmail.com>
2015-03-23 12:09:07 -0300

    Built Chirrin Chirrion toggles manager

--- lib/chirrin-chirrion.rb
+++ lib/chirrin-chirrion.rb
@@ -1,5 +1,38 @@
 require "chirrin-chirrion/version"
+require "chirrin-chirrion/database_adapters/redis_adapter"
 
 module ChirrinChirrion
-    # Your code goes here...
+  def self.config(options)
+    @database_adapter = options[:database_adapter]
+  end
+
+  def self.database_adapter
+    @database_adapter
+  end
+
+  def self.add_toggle(toggle_name)
+    database_adapter.add_toggle(toggle_name)
+  end
+
+  def self.remove_toggle(toggle_name)
+    database_adapter.remove_toggle(toggle_name)
+  end
+
+  def self.chirrin?(toggle_name)
+    database_adapter.exists?(toggle_name)
+  end
+
+  def self.chirrion?(toggle_name)
+    !chirrin?(toggle_name)
+  end
+
+  def self.chirrin_chirrion(toggle_name, for_chirrin, for_chirrion)
+    if chirrin?(toggle_name)
+      for_chirrin.respond_to?(:call) ? for_chirrin.call : for_chirrin
+    else
+      for_chirrion.respond_to?(:call) ? for_chirrion.call : for_chirrion
+    end
+  end
+
+  private_class_method :database_adapter
 end
--- spec/chirrin-chirrion_spec.rb
+++ spec/chirrin-chirrion_spec.rb
@@ -1,11 +1,138 @@
 require 'spec_helper'
 
 describe ChirrinChirrion do
-  it 'has a version number' do
-    expect(ChirrinChirrion::VERSION).not_to be nil
+  let(:database_adapter) { double }
+
+  describe ".add_toggle" do
+    let(:toggle_name) { 'my_toggle' }
+    before { ChirrinChirrion.config(database_adapter: database_adapter) }
+
+    context 'when the database adapter returns ok' do
+      before { allow(database_adapter).to receive(:add_toggle) { true } }
+
+      it 'returns true' do
+        expect(subject.add_toggle(toggle_name)).to eq(true)
+      end
+    end
+
+    context 'when the database adapter returns nok' do
+      before { allow(database_adapter).to receive(:add_toggle) { false } }
+
+      it 'returns false' do
+        expect(subject.add_toggle(toggle_name)).to eq(false)
+      end
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ".remove_toggle" do
+    let(:toggle_name) { 'my_toggle' }
+    before { ChirrinChirrion.config(database_adapter: database_adapter) }
+
+    context 'when the database adapter returns ok' do
+      before { allow(database_adapter).to receive(:remove_toggle) { true } }
+
+      it 'returns true' do
+        expect(subject.remove_toggle(toggle_name)).to eq(true)
+      end
+    end
+
+    context 'when the database adapter returns nok' do
+      before { allow(database_adapter).to receive(:remove_toggle) { false } }
+
+      it 'returns false' do
+        expect(subject.remove_toggle(toggle_name)).to eq(false)
+      end
+    end
+  end
+
+  describe ".chirrin?" do
+    let(:toggle_name) { 'my_toggle' }
+    before { ChirrinChirrion.config(database_adapter: database_adapter) }
+
+    context 'when the database adapter returns ok' do
+      before { allow(database_adapter).to receive(:exists?) { true } }
+
+      it 'returns true' do
+        expect(subject.chirrin?(toggle_name)).to eq(true)
+      end
+    end
+
+    context 'when the database adapter returns nok' do
+      before { allow(database_adapter).to receive(:exists?) { false } }
+
+      it 'returns false' do
+        expect(subject.chirrin?(toggle_name)).to eq(false)
+      end
+    end
+  end
+
+  describe ".chirrion?" do
+    let(:toggle_name) { 'my_toggle' }
+    before { ChirrinChirrion.config(database_adapter: database_adapter) }
+
+    context 'when the database adapter returns ok' do
+      before { allow(database_adapter).to receive(:exists?) { true } }
+
+      it 'returns false' do
+        expect(subject.chirrion?(toggle_name)).to eq(false)
+      end
+    end
+
+    context 'when the database adapter returns nok' do
+      before { allow(database_adapter).to receive(:exists?) { false } }
+
+      it 'returns true' do
+        expect(subject.chirrion?(toggle_name)).to eq(true)
+      end
+    end
+  end
+
+  describe ".chirrin_chirrion" do
+    let(:toggle_name) { 'my_toggle' }
+    before { ChirrinChirrion.config(database_adapter: database_adapter) }
+
+    context 'when the toggle is turned on' do
+      before { allow(database_adapter).to receive(:exists?) { true } }
+
+      context 'and instruction for chirrin is not a proc' do
+        let(:for_chirrin) { 'Margarida' }
+        let(:for_chirrion) { 'Este idiota' }
+
+        it 'returns for chirrin value' do
+          expect(subject.chirrin_chirrion(toggle_name, for_chirrin, for_chirrion)).to eq('Margarida')
+        end
+      end
+
+      context 'and instruction for chirrin is a proc' do
+        let(:for_chirrin) { lambda { 'Margarida, Chirrin!!!!' } }
+        let(:for_chirrion) { lambda { 'Este idiota, Chirrion!!!!' } }
+
+        it 'executes chirrin proc' do
+          expect(subject.chirrin_chirrion(toggle_name, for_chirrin, for_chirrion)).to eq('Margarida, Chirrin!!!!')
+        end
+      end
+    end
+
+    context 'when the database adapter returns nok' do
+      before { allow(database_adapter).to receive(:exists?) { false } }
+
+      context 'and instruction for chirrin is not a proc' do
+        let(:for_chirrin) { 'Margarida' }
+        let(:for_chirrion) { 'Este idiota' }
+
+        it 'returns for chirrion value' do
+          expect(subject.chirrin_chirrion(toggle_name, for_chirrin, for_chirrion)).to eq('Este idiota')
+        end
+      end
+
+      context 'and instruction for chirrin is a proc' do
+        let(:for_chirrin) { lambda { 'Margarida, Chirrin!!!!' } }
+        let(:for_chirrion) { lambda { 'Este idiota, Chirrion!!!!' } }
+
+        it 'executes chirrin proc' do
+          expect(subject.chirrin_chirrion(toggle_name, for_chirrin, for_chirrion)).to eq('Este idiota, Chirrion!!!!')
+        end
+      end
+    end
   end
 end
