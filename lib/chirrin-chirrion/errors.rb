cd1a81de51cabe6c9bb5df87b1b8eb4fddc29b3e
Bruno Vicenzo <bvicenzo@users.noreply.github.com>
2021-01-22 16:36:53 -0300

    Add Freezing comments (#9)
    Co-authored-by: Bruno Vicenzo <bruno@alumni.usp.br>

--- lib/chirrin-chirrion/errors.rb
+++ lib/chirrin-chirrion/errors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ChirrinChirrion
   module Errors
     ToggleNotFound   = Class.new(RuntimeError)
--- lib/chirrin-chirrion/version.rb
+++ lib/chirrin-chirrion/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ChirrinChirrion
   VERSION = "0.5.1"
 end
